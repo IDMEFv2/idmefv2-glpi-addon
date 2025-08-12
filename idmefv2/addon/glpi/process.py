e2cdd1857f4700619f531391b13fdb890b13abcc
François Déchelle <fdechelle@teclib.com>
2025-08-12 14:11:26 +0200

    Remove JSONPath; add DNS query

--- idmefv2/addon/glpi/process.py
+++ idmefv2/addon/glpi/process.py
@@ -1,74 +1,98 @@
 '''
 IDMEFv2 message processors
 '''
+
 import abc
+from typing import Any
 import dns.resolver
 import dns.reversename
-from typing import Any
-import jsonpath_ng as jsonpath
+
 
 class Processor(abc.ABC):
-    def __init__(self, context : Any):
+    def __init__(self, context: Any):
         self._context = context
 
+    def process(self, message: dict) -> dict:
+        if self.filter(message):
+            return self.transform(message)
+        return message
+
     @abc.abstractmethod
-    def process(self, message : dict) -> dict:
+    def filter(self, message: dict) -> bool:
         '''
-        Message processing method, implemented in sub-classes
+        Message filtering method, implemented in sub-classes
 
         Args:
             message (dict): the input IDMEFv2 message
 
         Returns:
-            dict: the processed message
+            bool: True if message must be further processed, false if not
 
         Raises:
             NotImplementedError: must be implemented in concrete sub-classes
         '''
         raise NotImplementedError()
 
-class NullProcessor(Processor):
-    def process(self, message : dict) -> dict:
+    def transform(self, message: dict) -> dict:
         '''
-        Message processing method
-        Does nothing
+        Message modification method, redefined in sub-classes
+        Base class implementation does nothing
 
         Args:
             message (dict): the input IDMEFv2 message
 
         Returns:
-            dict: the processed message
+            dict: the message
         '''
         return message
 
-class JSONPathProcessor(Processor):
-    def __init__(self, context: Any, path: str):
+
+class NullProcessor(Processor):
+    def filter(self, message: dict) -> bool:
+        return False
+
+
+class BaseDNSProcessor(Processor):
+    def __init__(self, context: Any, must_be_there: str, must_not_be_there: str):
         super().__init__(context)
-        self._jsonpath = jsonpath.parse(path)
+        self._must_be_there = must_be_there
+        self._must_not_be_there = must_not_be_there
 
-    def process(self, message: dict) -> dict:
-        if self._jsonpath.find(message):
-            return self.transform(message)
+    def filter(self, message: dict) -> bool:
+        for a in ['Source', 'Target']:
+            if isinstance(message.get(a), list):
+                for h in message[a]:
+                    if self._must_be_there in h and not self._must_not_be_there in h:
+                        return True
+        return False
+
+
+class ReverseDNSProcessor(BaseDNSProcessor):
+    def __init__(self, context : Any):
+        super().__init__(context, 'IP', 'Hostname')
+
+    def transform(self, message: dict) -> dict:
+        for a in ['Source', 'Target']:
+            for h in message[a]:
+                if 'Hostname' in h or not 'IP' in h:
+                    continue
+                addr = dns.reversename.from_address(h['IP'])
+                ptr = dns.resolver.resolve(addr, 'PTR')
+                if ptr:
+                    h['Hostname'] = str(ptr[0])
         return message
 
-    @abc.abstractmethod
-    def transform(self, message: dict) -> dict:
-        raise NotImplementedError()
 
-class ReverseDNSProcessor(JSONPathProcessor):
-    def __init__(self, context: Any):
-        super().__init__(context, '$.Source[*].IP')
+class DNSProcessor(BaseDNSProcessor):
+    def __init__(self, context : Any):
+        super().__init__(context, 'Hostname', 'IP')
 
     def transform(self, message: dict) -> dict:
-        for source in message['Source']:
-            if 'Hostname' in source:
-                continue
-            ip = source['IP']
-            addr = dns.reversename.from_address(ip)
-            ptr = dns.resolver.resolve(addr,'PTR')
-            if ptr:
-                source['Hostname'] = str(ptr[0])
+        for a in ['Source', 'Target']:
+            for h in message[a]:
+                if 'IP' in h or not 'Hostname' in h:
+                    continue
+                a = dns.resolver.resolve(h['Hostname'])
+                if a:
+                    h['IP'] = str(a[0])
         return message
-
-class DNSProcessor:
-    pass
--- idmefv2/addon/glpi/process_test.py
+++ idmefv2/addon/glpi/process_test.py
@@ -1,4 +1,4 @@
-from .process import NullProcessor, ReverseDNSProcessor
+from .process import NullProcessor, ReverseDNSProcessor, DNSProcessor
 
 def test_null():
     message = {'foo': 1, 'bar': 2}
@@ -36,8 +36,53 @@ IDMEFV2_1 =  {
     ],
 }
 
-def test_reverse_dns():
+IDMEFV2_2 =  {
+    'Version': '2.D.V04',
+    'ID': 'ead7600d-c954-4390-bc76-4cf94e509698',
+    'CreateTime': '2021-11-26T16:30:12.313039',
+    'Category': ['Recon.Scanning'],
+    'Priority': 'Low',
+    'Description' : '$.alert.category',
+    "Analyzer": {
+        "IP": "127.0.0.1",
+        "Name": "suricata",
+        "Model": "Suricata NIDS",
+        "Type": "Cyber",
+        "Category": [ "NIDS" ],
+        "Data": [ "Network"],
+        "Method": [ "Signature"]
+    },
+    'Source': [
+        {
+            'Hostname': 'dns.google.com',
+            'Port': ['9999'],
+            'Protocol': [ 'HTTP'],
+        },
+    ],
+    'Target': [
+        {
+            'Hostname': 'www.teclib.com',
+            'Port': [ 80],
+        },
+    ],
+}
+
+def test_reverse_dns_1():
     rp = ReverseDNSProcessor(None)
     o = rp.process(IDMEFV2_1)
     assert 'Hostname' in o.get('Source')[0]
     assert isinstance(o.get('Source')[0]['Hostname'], str)
+    assert 'google' in o.get('Source')[0]['Hostname']
+    assert 'Hostname' in o.get('Target')[0]
+    assert isinstance(o.get('Target')[0]['Hostname'], str)
+    assert 'gandi' in o.get('Target')[0]['Hostname']
+
+def test_dns_1():
+    p = DNSProcessor(None)
+    o = p.process(IDMEFV2_2)
+    assert 'IP' in o.get('Source')[0]
+    assert isinstance(o.get('Source')[0]['IP'], str)
+    assert '8.8.' in o.get('Source')[0]['IP']
+    assert 'IP' in o.get('Target')[0]
+    assert isinstance(o.get('Target')[0]['IP'], str)
+    assert '217.70.184.56' in o.get('Target')[0]['IP']

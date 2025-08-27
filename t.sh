33258d0b5559f5cc27ba949b3f4c76ff10e50891
François Déchelle <fdechelle@teclib.com>
2025-08-27 14:47:46 +0200

    Add elements

--- t.sh
+++ t.sh
@@ -1,1 +1,1 @@
-curl -X POST -H "Content-Type: application/json" -d '{"Source":[{"IP":"8.8.8.8"}]}' http://127.0.0.1:5000/dns
+curl -X POST -H "Content-Type: application/json" -d '{"Source":[{"IP":"192.168.1.11"}],"Target":[{"IP": "192.168.2.11"}]}' http://127.0.0.1:5000/dns

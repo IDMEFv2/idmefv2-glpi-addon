b3ad271724f1ab3da5682a67121d24a2bace3446
François Déchelle <fdechelle@teclib.com>
2025-09-01 17:03:59 +0200

    Add various requests

--- t.sh
+++ t.sh
@@ -1,1 +1,15 @@
-curl -X POST -H "Content-Type: application/json" -d '{"Source":[{"IP":"192.168.1.11"}],"Target":[{"IP": "192.168.2.11"}]}' http://127.0.0.1:5000/dns
+#!/bin/bash
+
+URL=http://127.0.0.1:5000
+
+function post()
+{
+    echo "POST request to ${URL}$2"
+    curl -X POST -H "Content-Type: application/json" -d "$1" ${URL}$2
+}
+
+post '{"Source":[{"IP":"192.168.1.11"}],"Target":[{"IP": "192.168.2.11"}]}' /null
+
+post '{"Source":[{"IP":"8.8.8.8"}],"Target":[{"Hostname":"www.teclib.com"}]}' /dns
+
+post '{"Source":[{"IP":"192.168.1.11"}],"Target":[{"IP": "192.168.2.11"}]}' /glpi

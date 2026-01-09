e0452d7e059ce6f3853f95bb308b6150f22d7d09
François Déchelle <fdechelle@teclib.com>
2026-01-09 11:02:41 +0100

    Update test script (usage)

--- tests/run-test.sh
+++ tests/run-test.sh
@@ -10,10 +10,15 @@ function usage()
     echo >&2 "Usage: $0 [-u ADDON_URL] [-t TESTSERVER_URL] IDMEFV2"
     echo >&2 "Send a IDMEFv2 message to the add-on server"
     echo >&2 "Examples:"
-    echo >&2 "$0 -u http://addon:5555/dns -t http://testserver:9999 test1.json"
-    echo >&2 "   Send test1.json to addon and send result to testserver"
-    echo >&2 "$0 -u http://addon:5555/glpi test2.json"
-    echo >&2 "   Send test2.json to addon without further validation"
+    echo >&2 "  $0 -u http://addon:5555/dns -t http://testserver:9999 test1.json"
+    echo >&2 "    Send test1.json to addon and send result to testserver"
+    echo >&2 "  $0 -u http://addon:5555/glpi test2.json"
+    echo >&2 "    Send test2.json to addon without further validation"
+    echo >&2 ""
+    echo >&2 "Defaults URL, when running this script on host and addon in docker:"
+    echo >&2 "  ADDON_URL=$DOCKER_ADDON_URL"
+    echo >&2 "  TESTSERVER_URL=$DOCKER_TESTSERVER_URL"
+    echo >&2 "Ports may change if customized using configuration file"
     echo >&2 ""
     echo >&2 "Arguments:"
     echo >&2 "  IDMEFV2             IDMEFv2 JSON file to send"
@@ -22,7 +27,6 @@ function usage()
     echo >&2 "  -u ADDON_URL        URL of add-on server to POST to"
     echo >&2 "  -t TESTSERVER_URL   URL of test server for message validation"
     echo >&2 "                      If no test server specified, message will not be sent for validation"
-    echo >&2 ""
 
     exit 1
 }
@@ -60,9 +64,10 @@ while getopts "u:t:h" opt; do
     esac
 done
 shift $((OPTIND-1))
+IDMEFV2=$1
+if [ -z "$IDMEFV2" -o -z "ADDON_URL" ] ; then usage; fi
 
 mkdir -p $TMPDIR
-IDMEFV2=$1
 PROCESSED_IDMEFV2=$(mktemp --tmpdir=$TMPDIR tmp.idmefv2.XXXXXXXXXX)
 process_file "$IDMEFV2" > "$PROCESSED_IDMEFV2"
 ADDON_RESPONSE=$(mktemp --tmpdir=$TMPDIR tmp.response.XXXXXXXXXX)
